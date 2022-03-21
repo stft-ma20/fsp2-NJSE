df55975d1dbccb6e40c66b774ae7dc93117139af
stft-ma20 <102089061+stft-ma20@users.noreply.github.com>
2022-03-21 22:36:09 +0200

    Delete mongoConnect.js

--- db/mongoConnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb+srv://ma-stfs-bep1-db:PWD4stfsbep1mdb@cluster0.vixgd.mongodb.net/spft-bep1');
-  console.log("mongo connected!!!");
-}
